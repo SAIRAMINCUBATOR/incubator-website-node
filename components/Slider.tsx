3297a3c0c842e8cb4b7b15dcd5210065adf40fca
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-02-29 11:10:18 +0530

    Added Shad CN

---
+++ components/Slider.tsx
@@ -0,0 +1,38 @@
+"use client"
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import img from "@/public/img.jpeg"
+import Image from "next/image"
+
+export const Slider = () => {
+    return (
+        <Carousel
+            opts={{
+                align: "start",
+                loop:true
+            }}
+            plugins={[
+                Autoplay({
+                    delay:2000,
+                }),
+            ]}
+            className="w-full "
+        >
+            <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="md:basis-full lg:basis-full w-full flex justify-center">
+                           <Image src={img} alt={"Image"} className={"h-[80%] w-full object-cover"}/>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+        </Carousel>
+    )
+}
